3296df3b00f73866e5b864dacda54ab271a7ce00
dlwub <dlwubi@gmail.com>
2023-03-28 22:14:19 +0300

    Setup linters

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
-    title { "MyString" }
-    text { "MyText" }
+    title { 'MyString' }
+    text { 'MyText' }
     commentscounter { 1 }
     likescounter { 1 }
     user { nil }
