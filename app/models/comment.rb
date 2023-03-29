5db6f44aa91acb86dd890a06eec012df84031c2d
dlwub <dlwubi@gmail.com>
2023-03-29 22:38:31 +0300

    Add methods that update counts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
+
+  after_save: update_comment_count
+
+  private 
+  def update_comment_count
+    post.update(commentscounter: post.comments.all.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: :post_id
   has_many :likes, foreign_key: :post_id
+
+  after_save :update_post_counter
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+  def update_post_counter
+    author.update(postscounter: author.posts.all.count)
+  end
 end
