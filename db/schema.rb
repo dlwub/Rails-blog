d9ade3f752d968cecb91f66eb613e8d4753b0111
dlwub <dlwubi@gmail.com>
2023-03-29 18:22:42 +0300

    Add association between models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_183004) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_035832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,10 +24,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_183004) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -59,7 +61,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_183004) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "users"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
