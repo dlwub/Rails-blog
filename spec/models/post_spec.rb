6fdc10bf947e41f8adc1ce494248d6d035937f4a
dlwub <dlwubi@gmail.com>
2023-03-30 22:16:53 +0300

    Add model specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,23 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  before :each do
-    @fifth_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                              bio: 'Teacher from Mexico.')
-    @sixth_post = Post.create(author: @fifth_user, title: 'Hello', text: 'This is my first post')
-
-    Comment.create(post: @sixth_post, author: @fifth_user, text: 'Hi Tom!')
-    Comment.create(post: @sixth_post, author: @fifth_user, text: 'A good start!')
-    Comment.create(post: @sixth_post, author: @fifth_user, text: 'Keep it up!')
-    Comment.create(post: @sixth_post, author: @fifth_user, text: 'Well done!')
-    Comment.create(post: @sixth_post, author: @fifth_user, text: 'exciting!')
-  end
-  it 'gives count of comments' do
-    expect(@sixth_post.recent_comment.length).to eq 5
-  end
-  it 'gives count of posts' do
-    expect(@fifth_user.posts_counter).to eq 2
-  end
   subject do
     Post.new(author: @fifth_user, title: 'Hello', text: 'This is my first post')
   end
@@ -37,23 +20,23 @@ RSpec.describe Post, type: :model do
       expect(subject.title).to eq 'Hello'
     end
 
-    it 'comments_counter should be integer' do
-      subject.comments_counter = 'a'
+    it 'commentscounter should be integer' do
+      subject.commentscounter = 'a'
       expect(subject).to_not be_valid
     end
 
-    it 'comments_counter should be integer greater than or equal to 0' do
-      subject.comments_counter = -1
+    it 'commentscounter should be integer greater than or equal to 0' do
+      subject.commentscounter = -1
       expect(subject).to_not be_valid
     end
 
-    it 'likes_counter should be integer' do
-      subject.likes_counter = 'a'
+    it 'likescounter should be integer' do
+      subject.likescounter = 'a'
       expect(subject).to_not be_valid
     end
 
-    it 'likes_counter should be integer greater than or equal to 0' do
-      subject.likes_counter = -1
+    it 'likescounter should be integer greater than or equal to 0' do
+      subject.likescounter = -1
       expect(subject).to_not be_valid
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before :each do
-    @sixth_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                              bio: 'Teacher from Mexico.')
-    Post.create(author: @sixth_user, title: 'Hello', text: 'This is my first post')
-    Post.create(author: @sixth_user, title: 'Hello', text: 'This is my first post')
-    Post.create(author: @sixth_user, title: 'Hello', text: 'This is my first post')
-  end
-
-  it 'gives count of posts' do
-    expect(@sixth_user.recent_posts.length).to eq 3
-  end
-
   subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
   before { subject.save }
   context 'testing validations' do
@@ -25,14 +13,15 @@ RSpec.describe User, type: :model do
       expect(subject.name).to eq 'Tom'
     end
 
-    it 'posts_counter should be integer' do
-      subject.posts_counter = 'a'
+    it 'postscounter should be integer' do
+      subject.postscounter = 'a'
       expect(subject).to_not be_valid
     end
 
-    it 'posts_counter should be integer greater than or equal to 0' do
-      subject.posts_counter = -1
+    it 'postscounter should be integer greater than or equal to 0' do
+      subject.postscounter = -1
       expect(subject).to_not be_valid
     end
   end
 end
+
