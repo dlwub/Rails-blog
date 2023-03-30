7b1c564f50b62c130e491af20a37e7e62fc7fbea
dlwub <dlwubi@gmail.com>
2023-03-30 20:52:36 +0300

    Add validations to User model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,20 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: :post_id
-  has_many :likes, foreign_key: :post_id
+	belongs_to :author, class_name: 'User'
+	has_many :comments, foreign_key: :post_id
+	has_many :likes, foreign_key: :post_id
 
-  after_save :update_post_counter
+	validates :title, presence: true, length: {maximum: 250}
+	validates :commentscounter, numericality: {only_integer: true}, comparison: {greater_than_or_equal_to: 0}
+	validates :likescounter, numericality: {only_integer: true}, comparison: {greater_than_or_equal_to: 0}
+	after_save :update_post_counter
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+	def recent_comments
+		comments.order(created_at: :desc).limit(5)
+	end
 
-  private
+	private
 
-  def update_post_counter
-    author.update(postscounter: author.posts.all.count)
-  end
+	def update_post_counter
+		author.update(postscounter: author.posts.all.count)
+	end
 end
