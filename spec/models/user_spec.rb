a8042650caf19d9b076013b625d333944c9d65da
dlwub <dlwubi@gmail.com>
2023-03-31 18:14:02 +0300

    Fix linter errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject{User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')}
-  
-  before{subject.save}
+  subject { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+  before { subject.save }
 
   it 'name should be present' do
-    subject.name = nil 
+    subject.name = nil
     expect(subject).to_not be_valid
   end
 
   it 'subject.name should return Tom' do
     expect(subject.name).to eq 'Tom'
-  end  
+  end
 end
