c6717cc11db7f5e37b9f90acda9254ddd913aac5
dlwub <dlwubi@gmail.com>
2023-04-07 20:49:08 +0300

    Generate Comments and Likes controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,26 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author = current_user
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      redirect_to user_post_path(current_user.id, @comment.post_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+  
 end
